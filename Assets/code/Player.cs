95994b98c6d23d8361172515ab55306ef6f4c27e
Mallchad <ncaprisunfan@gmail.com>
2020-02-13 10:58:13 +0000

    -Did stuff

--- Assets/code/Player.cs
+++ Assets/code/Player.cs
@@ -2,17 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+class Player : MonoBehaviour
 {
+    public Camera viewpoint;
+    Rigidbody rb;
+    Vector3 moveInput;
+    Vector3 moveVelocity;
     // Start is called before the first frame update
-    void Start()
+    void
+    Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
-
     // Update is called once per frame
-    void Update()
+    void
+    Update()
     {
-        
+	//Use a raycast to make the player face the direction of the mouse
+
+	// Create a new "plane" for our ray to "collide" with
+	Plane playerPlane = new Plane(Vector3.up, transform.position);
+	// Create the ray fro mthe camera towards the mouse position
+	Ray ray = this.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+	// Create a hit point for the distance to the position of where the ray hits hte plane
+	float hitPoint = 0f;
+	// Check to see if the plane is being hit by the ray, output the hit point distance
+	if (playerPlane.Raycast(ray, out hitPoint))
+	{
+	    // Create a vector to hold the coords of the hitpoint
+	    Vector3 targetPoint = ray.GetPoint(hitPoint);
+	    // Rotate the player to face the target point
+	    Quaternion targetRotation = Quaternion.LookRotation(targetPoint-transform.position);
+	    targetRotation.x = 0;
+	    targetRotation.z = 0;
+	    // Set the transform
+	    transform.rotation = Quaternion.Slerp(transform.rotation,
+						  targetRotation,
+						  7f * Time.deltaTime);
+	}
+
+	moveInput = new Vector3(Input.GetAxisRaw("Horizontal"),
+				0f,
+				Input.GetAxisRaw("Vertical"));
+	moveVelocity = moveInput * moveVelocity;
+    }
+    void
+    FixedUpdate()
+    {//
+	rb.velocity = moveVelocity;
     }
 }
--- Assets/code/ProjectileLauncher.cs
+++ Assets/code/ProjectileLauncher.cs
@@ -2,18 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class Gun : MonoBehaviour
 {
     // Start is called before the first frame update
-    void
-    Start()
+    void Start()
     {
         
     }
 
     // Update is called once per frame
-    void
-    Update()
+    void Update()
     {
         
     }
